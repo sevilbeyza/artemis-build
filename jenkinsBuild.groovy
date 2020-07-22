e3aa56871a0d19d9707e21fc92a616731f95d1f6
sevilbeyza <cankiroglus@gmail.com>
2020-07-22 17:26:04 -0500

    updated after Tav

---
+++ jenkinsBuild.groovy
@@ -0,0 +1,69 @@
+def k8slabel = "jenkins-pipeline-${UUID.randomUUID().toString()}"
+def slavePodTemplate = """
+      metadata:
+        labels:
+          k8s-label: ${k8slabel}
+        annotations:
+          jenkinsjoblabel: ${env.JOB_NAME}-${env.BUILD_NUMBER}
+      spec:
+        affinity:
+          podAntiAffinity:
+            requiredDuringSchedulingIgnoredDuringExecution:
+            - labelSelector:
+                matchExpressions:
+                - key: component
+                  operator: In
+                  values:
+                  - jenkins-jenkins-master
+              topologyKey: "kubernetes.io/hostname"
+        containers:
+        - name: docker
+          image: docker:latest
+          imagePullPolicy: IfNotPresent
+          command:
+          - cat
+          tty: true
+          volumeMounts:
+            - mountPath: /var/run/docker.sock
+              name: docker-sock
+        serviceAccountName: default
+        securityContext:
+          runAsUser: 0
+          fsGroup: 0
+        volumes:
+          - name: docker-sock
+            hostPath:
+              path: /var/run/docker.sock
+    """
+    def branch= "${scm.branches[0].name}".replaceAll(/^\*\//, '').replace("/", "-").toLowerCase() //get spesific baranch name -jenkin get branch name ?
+        podTemplate(name: k8slabel, label: k8slabel, yaml: slavePodTemplate, showRawYaml: false) {   
+            node(k8slabel) {
+            stage('Pull SCM') {
+                checkout scm
+            }
+            container('docker'){
+              dir('deployments/docker') {
+                stage("Docker Build") {
+                  sh "docker build -t sevil2020/artemis:${branch.replace('version/', 'v')}  ."
+                }
+                stage("Docker Login") {
+                 withCredentials([usernamePassword(credentialsId: 'docker credentials', passwordVariable: 'password', usernameVariable: 'username')]) {
+                sh "docker login --username ${username} --password ${password}"
+              }
+            }
+              stage("Docker Push") {
+                  sh "docker push sevil2020/artemis:${branch.replace('version/', 'v')}"
+              }
+              stage("Trigger Deploy"){
+                      build 'artemis-build'
+                      parameters: [
+                        [$class: 'BooleanParameterValue', name: 'terraformApply',     value: true],
+                        [$class: 'StringParameterValue',  name: 'environment',         value: "dev"]
+                  }
+               }
+            }
+         }
+      }
+}
+
+
---
+++ jenkinsDeploy.groovy
@@ -0,0 +1,13 @@
+properties([
+    parameters([
+        booleanParam(defaultValue: false, description: 'Please select to apply the changes', name: 'terraformApply'),
+        booleanParam(defaultValue: false, description: 'Please select to destroy everything.', name: 'terraformDestroy'),
+        booleanParam(defaultValue: false, description: 'Please select to run the job in debug mode', name: 'debugMode'),
+        choice(choices: ['dev', 'qa', 'stage', 'prod'], description: 'Please select the environment to deploy.', name: 'environment')
+        ])
+    ])
+
+println("""
+    Terraform apply: ${params.terraformApply}
+    Selected env : ${params.environment})
+println('Hello world')

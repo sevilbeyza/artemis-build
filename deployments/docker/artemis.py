61b97477e6f3aa27f9d5d36578a0c355c7102f12
sevilbeyza <cankiroglus@gmail.com>
2020-07-23 08:19:02 -0500

    update

--- deployments/docker/artemis.py
+++
@@ -1,9 +0,0 @@
-#flask code you can create back end 
-from flask import Flask
-from flask import render_template
-app = Flask(__name__)
-@app.route('/')
-def index():
-    return 'Welcome to Artemis E-commerse web application.'
-if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
